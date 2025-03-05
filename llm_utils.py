1fe24426c71b55e1806cb525e758347f74dea7a8
tanghaotian <coolguy67@stu.edu.cn>
2025-03-05 17:04:39 +0800

    增加apikey

--- llm_utils.py
+++ llm_utils.py
@@ -1,17 +1,17 @@
 from zhipuai import ZhipuAI
 
-api_key = ''
+api_key = '817be447d3b5470ba10349dde056a376.wN79bG2OhaUU2Cg3'
+
 
 def get_LLM_response(prompt):
-
-    client = ZhipuAI(api_key=api_key)  
+    client = ZhipuAI(api_key=api_key)
     response = client.chat.completions.create(
-        model="glm-4-flash", 
+        model="glm-4-flash",
         messages=[
             {"role": "user", "content": prompt},
         ],
     )
-    
+
     print(response.choices[0].message.content)
 
     return response.choices[0].message.content
