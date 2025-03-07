d76525499f4aebc6b5c47fb9e9cf529bc5a826df
tanghaotian <coolguy67@stu.edu.cn>
2025-03-07 14:08:41 +0800

    关于运动处方的ai调试

--- llm_utils.py
+++ llm_utils.py
@@ -6,8 +6,9 @@ api_key = '817be447d3b5470ba10349dde056a376.wN79bG2OhaUU2Cg3'
 def get_LLM_response(prompt):
     client = ZhipuAI(api_key=api_key)
     response = client.chat.completions.create(
-        model="glm-4-flash",
+        model="glm-4-plus",
         messages=[
+            {"role": "system", "content": "你是一个资深的医学专家，你的任务是根据用户输入的数据，为用户提供专业、准确的运动处方，包括运动项目、运动频率、运动强度，你的资料来源是PubMed。"},
             {"role": "user", "content": prompt},
         ],
     )
